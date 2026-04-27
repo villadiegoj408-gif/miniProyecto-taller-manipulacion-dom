fdfc05e0d302900d92afaed0a9e1485f5d074f18
villadiegoj408-gif <villadiegoj408@gmail.com>
2026-04-27 10:23:54 -0500

    feat: funcionalidad js

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+const numero = document.getElementById("numero");
+const btnAumentar = document.getElementById("aumentar");
+const btnDisminuir = document.getElementById("disminuir");
+
+let contador = 0;
+
+// función para cambiar color según valor
+function actualizarColor() {
+  if (contador > 0) {
+    numero.style.color = "green";
+  } else if (contador < 0) {
+    numero.style.color = "red";
+  } else {
+    numero.style.color = "black";
+  }
+}
+
+// sumar
+btnAumentar.addEventListener("click", () => {
+  contador++;
+  numero.textContent = contador;
+  actualizarColor();
+});
+
+// restar
+btnDisminuir.addEventListener("click", () => {
+  contador--;
+  numero.textContent = contador;
+  actualizarColor();
+});
